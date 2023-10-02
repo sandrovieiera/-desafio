57510dcce6abf60dc9755eedc858591aaf3833e8
sandrovieiera <sandro.sand172@gmailcom>
2023-10-02 09:21:10 -0300

    A Jornada Heroica

---
+++ aJornda/script.js
@@ -0,0 +1,8 @@
+const posicaoInicial = 9;
+const totalPassos = 5;
+
+//TODO: Calcule a posição final do herói:
+const posicaoFinal = posicaoInicial + totalPassos ;
+
+// Imprime a posição final
+console.log("Posicao final do heroi:", posicaoFinal);
